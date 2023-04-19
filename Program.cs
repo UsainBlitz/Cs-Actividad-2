e0a27525d09cc7c0aa9ca4483a21a5cf45f004e4
Alonso López <78806606+UsainBlitz@users.noreply.github.com>
2023-04-19 13:13:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Por favor, ingrese su nombre: ");
+        string nombre = Console.ReadLine();
+        Console.WriteLine("¡Hola " + nombre + "!");
+    }
+}
